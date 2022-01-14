eada563d20a8e12c6d8fadcbfc2ed092fb08066c
Aayush Garg <aayushgarg2006@gmail.com>
2022-01-14 20:38:14 +0530

    refactor

--- random.py
+++ random.py
@@ -1,8 +1,11 @@
 from datetime import datetime
-
 now = datetime.now().time() # time object
+
+
 def decimalToBinary(n):
     return int(bin(n).replace("0b", ""))
+def binaryToDecimal(n):
+    return int(n,2)
 
 state = decimalToBinary(int(str(now).split('.')[-1]))
 
@@ -11,7 +14,5 @@ for i in range(500):
     result.append(state & 1)
     newbit = (state ^ (state >> 3) ^ (state >> 12) ^ (state >> 14) ^ (state >> 15)) & 1
     state = (state >> 1) | (newbit << 15)
-def binaryToDecimal(n):
-    return int(n,2)
 
 print(binaryToDecimal(''.join(map(str, result))))
